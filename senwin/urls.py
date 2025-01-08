ed396c60f44089ee91626fce4382243e37ce2967
Godwin Umosen <umosengodwin568@gmail.com>
2025-01-08 04:41:07 -0800

    font-size: 50px;

--- senwin/urls.py
+++ senwin/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from . import views
 from .views import HomeView,ArticleDetailView,SecondProjectsDetailView,ContactView,Projects
-from .views import ProjectsDetailView,ThirdLatestPostDetailView
+from .views import ProjectsDetailView,ThirdLatestPostDetailView,SubPictureDetailView
 #from .views import
 
 urlpatterns = [
@@ -12,6 +12,7 @@ urlpatterns = [
     path('projects/', Projects.as_view(), name='projects'),
     path('article/<int:pk>/', ArticleDetailView.as_view(), name="detail"),
     path('article2/<int:pk>/', SecondProjectsDetailView.as_view(), name="second_detail"),
+    path('sub_picture/<int:pk>/', SubPictureDetailView.as_view(), name="sub_detail"),
     path('article3/<int:pk>/', ThirdLatestPostDetailView.as_view(), name="third_detail"),
     path('project_article/<int:pk>/', ProjectsDetailView.as_view(), name="project_article"),
     path('about_senwin/', views.AboutSenwin, name='about_senwin'),
